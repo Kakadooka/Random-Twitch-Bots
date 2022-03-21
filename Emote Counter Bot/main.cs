4856f0c5abb36b29c941c68721af40d15fd0124a
kakadooka <Dzemoreksteam@gmail.com>
2022-03-21 17:58:45 +0100

    counter and quote bots

---
+++ Emote Counter Bot/main.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using TwitchLib.Api.Core.Models.Undocumented.ClipChat;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+
+namespace OOOOvsMMMM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            Console.ReadLine();
+        }
+    }
+
+    public static class FishCount {
+        public static string file = "ooooCount.txt";
+        public static string userMessage;
+        public static string userId;
+        public static string userName;
+
+        public static int oooo;
+        public static int mmmm;
+
+        static string[] globalInfo = new string[3];
+        static string[] userInfo = new string[3];
+        static string[] quoteArray;
+
+        public static void Initialize()
+        {
+            if (!File.Exists(file)) {
+                using (var f = File.Create(file)) { }
+                using (StreamWriter writer = new StreamWriter(file)) { writer.WriteLine("Global-0-0"); }
+                return;
+            }
+        }
+
+
+        public static void UpdateList()
+        {
+
+
+            bool globalWritten = false;
+            bool userWritten = false;
+
+            using (StreamReader reader = new StreamReader(file)) { globalInfo = reader.ReadLine().Split("-", 3); }
+
+            quoteArray = File.ReadAllLines(file);
+            File.WriteAllText(file, null);
+
+
+
+            foreach (string s in quoteArray)
+            {
+                if (s.StartsWith(userId))
+                {
+                    userInfo = s.Split("-", 3);
+                }
+
+            }
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                {
+                    foreach (string s in quoteArray)
+                    {
+                        if (!globalWritten || !userWritten)
+                        {
+                            if (s.StartsWith("Global"))
+                            {
+                                writer.WriteLine("Global-" + (oooo + Convert.ToInt32(globalInfo[1])) + "-" + (mmmm + Convert.ToInt16(globalInfo[2])));
+                            }
+                            else if (s.StartsWith(userId))
+                            {
+                                writer.WriteLine(userId + "-" + (oooo + Convert.ToInt32(userInfo[1])) + "-" + (mmmm + Convert.ToInt16(userInfo[2])));
+                            }
+                            else
+                            {
+                                writer.WriteLine(s);
+                            }
+                        }
+                        else
+                        {
+                            writer.WriteLine(s);
+                        }
+                    }
+                    return;
+                }
+
+            }
+        }
+        public static void CheckIfUserExists()
+        {
+            string[] quoteArray = File.ReadAllLines(file);
+            bool userExists = false;
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                foreach (string s in quoteArray)
+                {
+                    if (s.StartsWith(userId))
+                    {
+                        userExists = true;
+                    }
+                    writer.WriteLine(s);
+                }
+
+                if (!userExists)
+                {
+                    writer.WriteLine(userId + "-0-0");
+                }
+            }
+            return;
+        }
+        public static void CheckWhichFish(ChatMessage chatMessage)
+        {
+            userMessage = chatMessage.Message;
+            userId = chatMessage.UserId;
+            userName = chatMessage.DisplayName;
+
+
+            if (userMessage.Contains("OOOO") && userMessage.Contains("MMMM"))
+            {
+                Console.WriteLine(DateTime.Now + userName + ": "+ userMessage + " | 1 1");
+                oooo = 1;
+                mmmm = 1;
+                CheckIfUserExists();
+                UpdateList();
+            }
+            else if (userMessage.Contains("OOOO"))
+            {
+                Console.WriteLine(DateTime.Now + userName + ": " + userMessage + " | 1 0");
+                oooo = 1;
+                mmmm = 0;
+                CheckIfUserExists();
+                UpdateList();
+            }
+            else if (userMessage.Contains("MMMM"))
+            {
+                Console.WriteLine(DateTime.Now + userName + ": " + userMessage + " | 0 1");
+                oooo = 0;
+                mmmm = 1;
+                CheckIfUserExists();
+                UpdateList();
+            }
+
+            return;
+        }
+
+        public static string CheckUserFishUsage(ChatMessage chatMessage)
+        {
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            Console.WriteLine(DateTime.Now + " | " + chatMessage.UserId + "-" + chatMessage.DisplayName + ": " + chatMessage.Message);
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+
+
+            quoteArray = File.ReadAllLines(file);
+            userId = chatMessage.UserId;
+            float ooooPercent;
+            float mmmmPercent;
+            bool stopReading = false;
+
+            foreach (string s in quoteArray)
+            {
+                if (s.StartsWith(userId))
+                {
+                    userInfo = s.Split("-", 3);
+                    stopReading = true;
+                }
+                if (stopReading)
+                {
+                    break;
+                }
+
+            }
+            if (!stopReading)
+            {
+                ooooPercent = 0;
+                mmmmPercent = 0;
+                userInfo[1] = "0";
+                userInfo[2] = "0";
+            }
+            else
+            {
+                ooooPercent = float.Parse(userInfo[1]) / (float.Parse(userInfo[1]) + float.Parse(userInfo[2])) * 100;
+                mmmmPercent = float.Parse(userInfo[2]) / (float.Parse(userInfo[1]) + float.Parse(userInfo[2])) * 100;
+            }
+
+
+            if (ooooPercent != ooooPercent)
+            {
+                ooooPercent = 0;
+            }
+            if (mmmmPercent != mmmmPercent)
+            {
+                mmmmPercent = 0;
+            }
+
+            return "@" + chatMessage.DisplayName + " has used OOOO " + userInfo[1] + " times (" + Math.Round(ooooPercent, 1) + "%) and MMMM " + userInfo[2] + " times (" + Math.Round(mmmmPercent, 1) + "%)";
+        }
+
+        public static string CheckGlobalFishUsage(ChatMessage chatMessage)
+        {
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            Console.WriteLine(DateTime.Now + " | " + chatMessage.UserId + "-" + chatMessage.DisplayName + ": " + chatMessage.Message);
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            Console.WriteLine("COMMAND USED OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+
+            quoteArray = File.ReadAllLines(file);
+            float ooooPercent;
+            float mmmmPercent;
+            bool stopReading = false;
+
+            foreach (string s in quoteArray)
+            {
+                if (s.StartsWith("Global"))
+                {
+                    globalInfo = s.Split("-", 3);
+                    stopReading = true;
+                }
+                if (stopReading)
+                {
+                    break;
+                }
+
+            }
+
+            ooooPercent = float.Parse(globalInfo[1]) / (float.Parse(globalInfo[1]) + float.Parse(globalInfo[2])) * 100;
+            mmmmPercent = float.Parse(globalInfo[2]) / (float.Parse(globalInfo[1]) + float.Parse(globalInfo[2])) * 100;
+
+
+            return "OOOO has been used " + globalInfo[1] + " times (" + Math.Round(ooooPercent, 1) + "%) and MMMM " + globalInfo[2] + " times (" + Math.Round(mmmmPercent, 1) + "%)";
+        }
+    }
+
+    class Bot
+    {
+        TwitchClient client;
+        string twitchChannel = "channel"; //change this to whatever channel you want your bot to join
+
+        public Bot()
+        {
+            
+            ConnectionCredentials credentials = new ConnectionCredentials("botName", "oAuth"); //change this to your bots name and its oAuth key
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+
+            };
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+            client = new TwitchClient(customClient);
+            client.Initialize(credentials, twitchChannel);
+
+            client.OnMessageReceived += Client_OnMessageReceived;
+            client.OnConnected += Client_OnConnected;
+
+            FishCount.Initialize();
+            client.Connect();
+        }
+
+        //Commands
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {         
+
+            switch (e.ChatMessage.Message)
+            {
+                
+                case "!fishbalance":
+                    client.SendMessage(twitchChannel, FishCount.CheckUserFishUsage(e.ChatMessage));               
+                    break;
+                case "!fishglobal":
+                    client.SendMessage(twitchChannel, FishCount.CheckGlobalFishUsage(e.ChatMessage));
+                    break;
+
+                /*case "!test":
+                    if (test)
+                    {
+                        client.SendMessage(twitchChannel, ("0"));
+                        test = false;
+                    }
+                    else
+                    {
+                        client.SendMessage(twitchChannel, ("1"));
+                        test = true;
+                    }
+                    break;*/
+            }
+
+            FishCount.CheckWhichFish(e.ChatMessage);
+        }
+
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {
+            Console.WriteLine($"Connected to {e.AutoJoinChannel}");
+        }
+
+
+
+    }
+}
---
+++ Quote Bot/main.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.IO;
+
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+
+namespace TwitchBot
+{
+    public static class FISH
+    {
+        static public string FIIIIIIIISH()
+        {
+            return "kinda weird... \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⠀⠀ \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⡴⠶⠞⠛⠋⠉⠉⠉⠉⠛⠷ \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡤⠶⠞⠛⠛⠉⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣶⣶ \n⠀⠀⠀⠀⠀⠀⢀⠜⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⣠⣤⣾⡋⠀⠈⠻⢿⣿ \n⠀⠀⠀⠀⠀⢀⠂⠀⠀⠀⠀⠀⠀⢀⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⠖⣶⣤⣀⣿ \n⠀⠀⠀⠀⢀⠊⠀⠀⠀⠀⠀⠀⠀⠙⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣀⣿⣿⣿⣿ \n⠀⠀⢀⠌⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ \n⠀⠀⢸⠀⠀⠀⢀⣤⣄⣀⣀⣀⣀⣠⣤⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ \n⠀⠀⠘⢄⠀⠀⠘⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ \n⠀⠀⠀⠀⠈⠒⠤⣀⣀⣀⣀⣀⣀⣀⣀⣀⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ \n⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ \n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠛";
+        }
+    }
+
+    public static class QuoteList
+    {
+        static int quoteAmount;
+        static int convertedNumber;
+
+        static string quote;
+        static string file = "Quotes.quotes";
+        static string[] messageParts;
+
+        static bool errorCheck;
+        static string errorMessage;
+
+        static public void CheckConvertedNumber(int convertedNumber, int quoteAmount)
+        {
+            if (convertedNumber < 1 || convertedNumber > quoteAmount)
+            {
+                errorMessage = "There's no quote #" + convertedNumber;
+                errorCheck = true;
+            }
+            return;
+        }
+        //************************************************//
+        static public int CalculateQuoteAmount()
+        {
+            using (StreamReader reader = new StreamReader(file))
+            {
+                quoteAmount = 0;
+                while (reader.ReadLine() != null)
+                {
+                    quoteAmount++;
+                }
+            }
+            return quoteAmount;
+        }
+        //************************************************//
+        static public void CheckFileExists()
+        {
+            if (!File.Exists(file))
+            {
+                File.Create(file);
+                errorMessage = "There are no quotes!";
+                errorCheck = true;
+            }
+            return;
+        }
+        //************************************************//
+        static public int CheckNumber(string number)
+        {
+            try
+            {
+                convertedNumber = int.Parse(number);
+            }
+            catch (SystemException)
+            {
+                errorMessage = "That's not a number!";
+                errorCheck = true;
+                return convertedNumber;
+            }
+
+            convertedNumber = int.Parse(number);
+
+            try 
+            { 
+                if (convertedNumber.GetType() != typeof(int)) { } 
+            }
+            catch (SystemException)
+            { 
+                errorMessage = "That's not a number!";
+                errorCheck = true;
+            }
+
+            if (convertedNumber.GetType() != typeof(int))
+            {
+                errorMessage = "That's not a number!";
+                errorCheck = true;
+            }
+
+            return convertedNumber;
+        }
+        //************************************************//
+        static public void CheckFileEmpty()
+        {
+            using (StreamReader reader = new StreamReader(file))
+            {
+                if (reader.ReadLine() == null)
+                {
+                    errorMessage = "There are no quotes!";
+                    errorCheck = true;
+                }
+            }
+            return;
+        }
+
+        //************************************************//
+        //************************************************//
+        static public string AddQuote(ChatMessage message)
+        {
+            errorCheck = false;
+
+            quote = message.Message.Replace("!addquote ", "");
+
+            CheckFileEmpty();
+            quoteAmount = CalculateQuoteAmount();
+            if (errorCheck) { return errorMessage; }
+
+            using (StreamWriter writer = File.AppendText(file))
+            {
+                if (!File.Exists(file))
+                {
+                    File.Create(file);
+                }
+
+                writer.WriteLine(quote);
+            }
+
+            return "Added quote : " + quote + " | (" + (quoteAmount + 1) + "/" + (quoteAmount + 1) + ")";
+        }
+        //************************************************//
+        static public string ReadRandomQuote()
+        {
+            errorCheck = false;
+
+            Random randomVariable = new Random();
+            int randomNum;
+
+            CheckFileEmpty();
+            CheckFileExists();
+            quoteAmount = CalculateQuoteAmount();
+            if (errorCheck) { return errorMessage; }
+
+            randomNum = randomVariable.Next(quoteAmount);
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+                for (int i = 0; i < (randomNum + 1); i++)
+                {
+                    quote = reader.ReadLine() + " | (" + (randomNum + 1) + "/" + quoteAmount + ")";
+                }
+            }
+            return quote;
+        }
+        //************************************************//
+        public static string ReadSpecificQuote(ChatMessage message)
+        {
+            errorCheck = false;
+
+            messageParts = message.Message.Split(" ", 3);
+
+            CheckFileExists();
+            CheckFileEmpty();
+            convertedNumber = CheckNumber(messageParts[1]);
+            quoteAmount = CalculateQuoteAmount();
+            CheckConvertedNumber(convertedNumber, quoteAmount);
+            if (errorCheck) { return errorMessage; }
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+                int CurrentQuote = 0;
+                for (int i = 0; i < convertedNumber; i++)
+                {
+                    CurrentQuote++;
+                    quote = reader.ReadLine() + " | (" + CurrentQuote + "/" + quoteAmount + ")";
+                }
+            }
+            return quote;
+        }
+        //************************************************//
+        public static string DeleteQuote(ChatMessage message)
+        {
+            string[] quoteArray;
+
+            errorCheck = false;
+            messageParts = message.Message.Split(" ", 3);
+
+            CheckFileExists();
+            CheckFileEmpty();
+            convertedNumber = CheckNumber(messageParts[1]);
+            quoteAmount = CalculateQuoteAmount();
+            CheckConvertedNumber(convertedNumber, quoteAmount);
+            if (errorCheck) { return errorMessage; }
+
+            quoteArray = File.ReadAllLines(file);
+            quoteArray[convertedNumber - 1] = null;
+
+            File.WriteAllText(file, null);
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                foreach (string s in quoteArray)
+                {
+                    if (s != null)
+                    {
+                        writer.WriteLine(s);
+                    }
+                }
+            }
+            return "Deleted quote #" + convertedNumber;
+        }
+        //************************************************//
+        public static string ReplaceQuote(ChatMessage message)
+        {
+            string[] quoteArray;
+
+            errorCheck = false;
+            messageParts = message.Message.Split(" ", 3);
+
+            CheckFileExists();
+            CheckFileEmpty();
+            convertedNumber = CheckNumber(messageParts[1]);
+            quoteAmount = CalculateQuoteAmount();
+            CheckConvertedNumber(convertedNumber, quoteAmount);
+
+            if (errorCheck) {return errorMessage;}
+
+            quoteArray = File.ReadAllLines(file);
+            quoteArray[convertedNumber - 1] = messageParts[2];
+
+            File.WriteAllText(file, null);
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                foreach (string s in quoteArray)
+                {
+                    if (s != messageParts[2])
+                    {
+                        writer.WriteLine(s);
+                    }
+                    else {
+                        writer.WriteLine(messageParts[2]);
+                    }
+                }
+                return "Edited quote #" + messageParts[1];
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            Console.ReadLine();
+        }
+    }
+    class Bot
+    {
+        TwitchClient client;
+        string twitchChannel = "channel"; //channel should be changed to whatever channel you want the bot to join.
+
+        public Bot()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials("botName", "authKey");
+            //botName should be changed to the twitch account name of your bot, and
+            //authkey should be changed to your bots authentication key, which can be generated using twitchtokengenerator.com
+
+            if (twitchChannel == "channel")
+            {
+                Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                Console.WriteLine("Please change variables twitchChannel, botName, authKey in the code!");
+                return;
+            }
+
+
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+            client = new TwitchClient(customClient);
+            client.Initialize(credentials, twitchChannel);
+
+            client.OnLog += Client_OnLog;
+            client.OnJoinedChannel += Client_OnJoinedChannel;
+            client.OnMessageReceived += Client_OnMessageReceived;
+            client.OnConnected += Client_OnConnected;
+
+            client.Connect();
+        }
+
+        //commands
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+
+            if (e.ChatMessage.IsModerator || e.ChatMessage.IsBroadcaster)
+            {
+                if (e.ChatMessage.Message.StartsWith("!addquote "))
+                {
+                    client.SendMessage(twitchChannel, QuoteList.AddQuote(e.ChatMessage));
+                }
+                else if (e.ChatMessage.Message.StartsWith("!deletequote "))
+                {
+                    client.SendMessage(twitchChannel, QuoteList.DeleteQuote(e.ChatMessage));
+                }
+                else if (e.ChatMessage.Message.StartsWith("!editquote "))
+                {
+                    client.SendMessage(twitchChannel, QuoteList.ReplaceQuote(e.ChatMessage));
+                }
+            }
+
+            if (e.ChatMessage.IsVip || e.ChatMessage.IsModerator || e.ChatMessage.IsBroadcaster)
+            {
+                if (e.ChatMessage.Message.StartsWith("!readquote "))
+                {
+                    client.SendMessage(twitchChannel, QuoteList.ReadSpecificQuote(e.ChatMessage));
+                }
+
+                switch (e.ChatMessage.Message)
+                {
+                    case "!weird":
+                        client.SendMessage(twitchChannel, FISH.FIIIIIIIISH());
+                        break;
+                }
+            }
+
+
+            //regular user commands
+            switch (e.ChatMessage.Message)
+            {
+                case "!quote":
+                    client.SendMessage(twitchChannel, QuoteList.ReadRandomQuote());
+                    break;
+                case "id":
+                    client.SendMessage(twitchChannel, e.ChatMessage.UserId);
+                    break;
+            }
+        }
+
+        private void Client_OnLog(object sender, OnLogArgs e)
+        {
+            Console.WriteLine($"{e.DateTime.ToString()}: {e.BotUsername} - {e.Data}");
+        }
+
+        private void Client_OnConnected(object sender, OnConnectedArgs e)
+        {         
+            Console.WriteLine($"Connected to {e.AutoJoinChannel}");
+        }
+
+        private void Client_OnJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            client.SendMessage(e.Channel, "Joined " + twitchChannel + " | Bot version 0.0");
+        }
+    }
+}
